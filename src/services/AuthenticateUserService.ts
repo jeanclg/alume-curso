8d0a6b228ccd116b41196e3f2a5ee9dbcbb943be
Jean Garcia <jean_clg@hotmail.com>
2021-09-05 11:22:50 -0300

    create auth token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -25,7 +27,12 @@ class AuthenticateUserService {
     if (!passwordMatched)
       throw new Error('Incorrect email/password combination.');
 
-    return { user };
+    const token = sign({}, '7138bfeeac81dcebcbfa579205878612', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
